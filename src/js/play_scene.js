5d487a3925cdfdb8b949170b98a760c17d388c1c
Kimmo Peura <kimmo.peura@karzasol.fi>
2017-01-22 13:30:02 +0200

    gameStop on Death

--- src/js/play_scene.js
+++ src/js/play_scene.js
@@ -9,8 +9,8 @@ var PlayScene = {
   sounds: {
     explosion: undefined
   },
-  
-  
+
+
   preload: function () {
     this.load.image('player', 'images/player/ship.png');
     this.load.image('bullet1', 'images/player/bullet1.png');
@@ -18,6 +18,7 @@ var PlayScene = {
   },
 
   create: function () {
+    var thisGame = this.game;
     BackgroundController = require('./BackgroundController.js');
     EnemyController = require('./EnemyController.js');
     PlayerController = require('./PlayerController.js');
@@ -25,39 +26,52 @@ var PlayScene = {
     this.sounds["explosion"] = this.game.add.audio('explosion');
 
     Effects.create(this.game);
-    
+
     PlayerController.create(this.game, this.input);
     BackgroundController.create(this.game, PlayerController);
-    
+
     EnemyController.create(this.game, BackgroundController.mountainCollisionGroup);
-    BackgroundController.topGroup.body.collides(EnemyController.enemyCollisionGroup, function(mountain, enemy) {
+    BackgroundController.topGroup.body.collides(EnemyController.enemyCollisionGroup, function (mountain, enemy) {
+      Effects.explode(enemy.sprite, "explosion", function () {
+        thisGame.paused = true;
+        thisGame.input.keyboard.onDownCallback = function (e) {
+            location.reload();
+        }});
+      this.sounds["explosion"].play();
+    }, this);
+
+    PlayerController.player.body.collides(BackgroundController.mountainCollisionGroup, function (player, mountain) {
+      Effects.explode(player.sprite, "explosion", function () {
+        thisGame.paused = true;
+        thisGame.input.keyboard.onDownCallback = function (e) {
+            location.reload();
+        }
+      });
+      this.sounds["explosion"].play();
+    }, this);
+
+    PlayerController.player.body.collides(EnemyController.enemyCollisionGroup, function (player, enemy) {
+      Effects.explode(player.sprite, "explosion", function () {
+        thisGame.paused = true;
+        thisGame.input.keyboard.onDownCallback = function (e) {
+            location.reload();
+        }});
       Effects.explode(enemy.sprite, "explosion");
       this.sounds["explosion"].play();
     }, this);
-    
-    PlayerController.player.body.collides(BackgroundController.mountainCollisionGroup, function(player, mountain) {
-      Effects.explode(player.sprite, "explosion");
-      this.sounds["explosion"].play();
-     }, this);
-    
-    PlayerController.player.body.collides(EnemyController.enemyCollisionGroup, function(player, enemy) {
-      Effects.explode(player.sprite, "explosion");
-      Effects.explode(enemy.sprite, "explosion");
-      this.sounds["explosion"].play();
-    }, this);
-    
+
     PlayerController.collideBullets(EnemyController.enemyCollisionGroup);
   },
-  update: function() {
-    
+  update: function () {
+
     BackgroundController.update(this.game);
     EnemyController.update(this.game, PlayerController, BackgroundController.time);
     PlayerController.update(this.game);
 
-    if(this.input.keyboard.isDown(Phaser.Keyboard.ESC)) {
+    if (this.input.keyboard.isDown(Phaser.Keyboard.ESC)) {
       location.reload();
     }
-    
+
   },
 
 };
