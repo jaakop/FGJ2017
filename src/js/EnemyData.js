f209b1527c7f7f124cece1720c2af8ae23d5db3d
Kimmo Peura <kimmo.peura@karzasol.fi>
2017-01-22 11:48:15 +0200

    enemy data upgrade

--- src/js/EnemyData.js
+++ src/js/EnemyData.js
@@ -1,206 +1,402 @@
 var EnemyData = [
+  // WAVE1
   {
-      type: "boss",
-      spriteName: "vihu3",
-      time: 10,
-      posy: 350,
-      
-      gravityMultiplier: 1,
-      sprite: undefined
-    },
-    {
-      type: "boss",
-      spriteName: "vihu3",
-      time: 400,
-      posy: 150,
-      
-      gravityMultiplier: 1,
-      sprite: undefined
-    },{
-      type: "boss",
-      spriteName: "vihu3",
-      time: 400,
-      posy: 550,
-      
-      gravityMultiplier: 1,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 50,
-      posy: 50,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 50,
-      posy: 75,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 50,
-      posy: 100,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 50,
-      posy: 550,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 50,
-      posy: 575,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 50,
-      posy: 600,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "dragger",
-      spriteName: "vihu1",
-      time: 700,
-      posy: 200,
-      frequency: 200,
-      gravityMultiplier: 5,
-      sprite: undefined
-    },{
-      type: "dragger",
-      spriteName: "vihu1",
-      time: 700,
-      posy: 400,
-      frequency: 200,
-      gravityMultiplier: 5,
-      sprite: undefined
-    },{
-      type: "dragger",
-      spriteName: "vihu1",
-      time: 700,
-      posy: 600,
-      frequency: 200,
-      gravityMultiplier: 5,
-      sprite: undefined
-    },{
-      type: "boss",
-      spriteName: "vihu3",
-      time: 1000,
-      posy: 450,
-      
-      gravityMultiplier: 1,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1000,
-      posy: 300,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1000,
-      posy: 325,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1000,
-      posy: 350,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1000,
-      posy: 375,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1100,
-      posy: 400,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1100,
-      posy: 425,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1200,
-      posy: 375,
-      
-      gravityMutiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1200,
-      posy: 400,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "follower",
-      spriteName: "vihu2",
-      time: 1200,
-      posy: 425,
-      
-      gravityMultiplier: 0.5,
-      sprite: undefined
-    },{
-      type: "dragger",
-      spriteName: "vihu1",
-      time: 1500,
-      posy: 150,
-      
-      gravityMultiplier: 1,
-      sprite: undefined
-    },{
-      type: "dragger",
-      spriteName: "vihu1",
-      time: 1500,
-      posy: 200,
-      
-      gravityMultiplier: 1,
-      sprite: undefined
-    },{
-      type: "dragger",
-      spriteName: "vihu1",
-      time: 1500,
-      posy: 250,
-      
-      gravityMultiplier: 1,
-      sprite: undefined
-    }
+    type: "follower",
+    spriteName: "vihu2",
+    time: 10,
+    posy: 50,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 10,
+    posy: 600,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 50,
+    posy: 150,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 50,
+    posy: 450,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 80,
+    posy: 320,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+
+  // WAVE2
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 170,
+    posy: 500,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 200,
+    posy: 550,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 230,
+    posy: 600,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 260,
+    posy: 300,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 265,
+    posy: 50,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+
+  // WAVE3
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 400,
+    posy: 200,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 400,
+    posy: 400,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 420,
+    posy: 190,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 420,
+    posy: 390,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 450,
+    posy: 200,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 450,
+    posy: 400,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 480,
+    posy: 190,
+    frequency: 180,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 480,
+    posy: 390,
+    frequency: 180,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 510,
+    posy: 200,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 510,
+    posy: 400,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 550,
+    posy: 190,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 550,
+    posy: 390,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+
+  // WAVE4
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 750,
+    posy: 100,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 800,
+    posy: 150,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 850,
+    posy: 200,
+    frequency: 210,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 900,
+    posy: 250,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 950,
+    posy: 300,
+    frequency: 190,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 1000,
+    posy: 350,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 1050,
+    posy: 400,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 1100,
+    posy: 450,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 1150,
+    posy: 500,
+    frequency: 220,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 1200,
+    posy: 550,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 1250,
+    posy: 600,
+    frequency: 180,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 1300,
+    posy: 650,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+
+  // WAVE5
+  {
+    type: "boss",
+    spriteName: "vihu3",
+    time: 1600,
+    posy: 350,
+
+    gravityMultiplier: 1,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 1650,
+    posy: 50,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+  {
+    type: "follower",
+    spriteName: "vihu2",
+    time: 1650,
+    posy: 650,
+
+    gravityMultiplier: 0.5,
+    sprite: undefined
+  },
+
+  // WAVE6
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 2000,
+    posy: 500,
+    frequency: 200,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 2060,
+    posy: 520,
+    frequency: 170,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "boss",
+    spriteName: "vihu3",
+    time: 2120,
+    posy: 550,
+
+    gravityMultiplier: 1,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 2150,
+    posy: 300,
+    frequency: 180,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "dragger",
+    spriteName: "vihu1",
+    time: 2060,
+    posy: 520,
+    frequency: 170,
+    gravityMultiplier: 5,
+    sprite: undefined
+  },
+  {
+    type: "boss",
+    spriteName: "vihu3",
+    time: 2120,
+    posy: 550,
+
+    gravityMultiplier: 1,
+    sprite: undefined
+  },
 ];
 
 
